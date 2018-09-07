c10968859f89bfdd4fc0c0f7720671fa815b8a6c
alt440 <35940672+alt440@users.noreply.github.com>
2018-09-07 12:55:20 -0400

    Update DicesRoll.cs

--- DicesRoll.cs
+++ DicesRoll.cs
@@ -3,21 +3,31 @@ using System.Collections;
 
 public class DicesRoll : MonoBehaviour {
 
+	//this class is set to a dice object
+	//the value indicated by the dice
     public int valueOfDice;
+	//set when it is the beginning of a player's turn
     public bool readyForMove = false;
+	//speed of dice thrown
     Vector3 speed;
+	//camera object on the dice
     public GameObject camera;
+	//main player of the game
     public MovePlayer player;
 
 	// Use this for initialization
 	void Start () {
 
+		//if the dice are not currently used
         if (gameObject.activeInHierarchy == false)
         {
+		//they are activated
             gameObject.SetActive(true);
         }
 
+		//to let the dice stay on the board
         Physics.gravity= new Vector3(0, -300);
+		//sets velocity and angular velocity of the dice throw
         GetComponent<Rigidbody>().velocity = new Vector3(0, 0,Random.Range(-300,-500));
         GetComponent<Rigidbody>().angularVelocity = Random.insideUnitSphere * Random.Range(1,10); 
 	}
@@ -49,6 +59,7 @@ public class DicesRoll : MonoBehaviour {
     void MovePlayerValue()
     {
 
+	    //this method finds the value the dice indicate
         if (transform.localEulerAngles.z >89 &&  transform.localEulerAngles.z <91)
         {
             valueOfDice = 5;
@@ -83,12 +94,16 @@ public class DicesRoll : MonoBehaviour {
 
     public void RestartRolling(MovePlayer player)
     {
+	    //if the dice are not doing anything right now
         if (gameObject.activeInHierarchy == false)
         {
+		//see the dice
             gameObject.SetActive(true);
         }
 
+	    //sets the gravity so the players do not jump everytime they are moved
         Physics.gravity = new Vector3(0, -300);
+	    //sets the direction of the dice, their velocity and angular velocity when they are thrown
         if (player.index < 9 || player.index == 39)
         {
             GetComponent<Rigidbody>().velocity = new Vector3(0, -100, Random.Range(-300, -500));
@@ -114,12 +129,15 @@ public class DicesRoll : MonoBehaviour {
 
     public void RestartRolling(MoveAI player)
     {
+	    //if the dice is not doing anything right now
         if (gameObject.activeInHierarchy == false)
         {
             gameObject.SetActive(true);
         }
 
+	    //setss the gravity so the object stays on the board and does not jump everytime it moves
         Physics.gravity = new Vector3(0, -300);
+	    //this below gives the direction of the throw of dice and both the velocity and angular velocity of the dice
         if (player.index < 9 || player.index == 39)
         {
             GetComponent<Rigidbody>().velocity = new Vector3(0, -100, Random.Range(-300, -500));
