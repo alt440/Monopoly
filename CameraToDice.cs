c6110cc37cdc547c8153ac3749fd6dd664244b58
alt440 <35940672+alt440@users.noreply.github.com>
2018-09-07 12:35:02 -0400

    Update CameraToDice.cs

--- CameraToDice.cs
+++ CameraToDice.cs
@@ -3,15 +3,20 @@ using System.Collections;
 
 public class CameraToDice : MonoBehaviour {
 
+    //the dice object
     public GameObject dice;
+    //the camera object that follows the dice
     public GameObject camera;
+    //the previous player position
     Vector3 outdatedPlayerPos;
     float Tparam;
+    //the desired location of the camera after the dice move
     Vector3 desiredLocationCamera;
     public float speedCam;
 
     // Use this for initialization
     void Start () {
+        //initial position of the dice camera
         desiredLocationCamera = new Vector3(dice.transform.position.x, dice.transform.position.y + 300, dice.transform.position.z);
         outdatedPlayerPos = dice.transform.position;
 
@@ -20,6 +25,7 @@ public class CameraToDice : MonoBehaviour {
     // Update is called once per frame
     void Update () {
 
+        //this is for the dice camera to be constantly above of the dice.
         if (dice.transform.position.x > outdatedPlayerPos.x + 1 || dice.transform.position.x < outdatedPlayerPos.x - 1
             || dice.transform.position.z > outdatedPlayerPos.z + 1 || dice.transform.position.z < outdatedPlayerPos.z - 1)
         {
@@ -32,6 +38,7 @@ public class CameraToDice : MonoBehaviour {
 
     IEnumerator MoveFct()
     {
+        //this is the animation that moves the camera so that it follows the dice.
         float timeSinceStart = 0f;
         while (true)
         {
